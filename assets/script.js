0931fe81b9d3e7d1553476c7b03149c40734a344
Jared <Jared@Littles-MacBook-Pro.local>
2017-12-06 23:51:36 -0500

    onClick

--- assets/script.js
+++ assets/script.js
@@ -1,26 +1,13 @@
 //source of the gifs
 
       
-      var topics = ['Donald Trump', 'Barack Obama', 'George W. Bush', 'Bill Clinton', 'George Bush', 'Ronald Reagan', 'Jimmy Carter'];
+      var topics = ['Donald Trump', 'Barack Obama', 'George H.W. Bush', 'Bill Clinton', 'George Bush', 'Ronald Reagan', 'Jimmy Carter'];
       var president;
 	  var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
       		president + "&api_key=dc6zaTOxFJmzC&limit=10";
       //var person = "taylor swift";
       //var i;
- $(".gif").on("click", function() {
-      // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
-      var state = $(this).attr("data-state");
-      // If the clicked image's state is still, update its src attribute to what its data-animate value is.
-      // Then, set the image's data-state to animate
-      // Else set src to the data-still value
-      if (state === "still") {
-        $(this).attr("src", $(this).attr("data-animate"));
-        $(this).attr("data-state", "animate");
-      } else {
-        $(this).attr("src", $(this).attr("data-still"));
-        $(this).attr("data-state", "still");
-      }
-    });
+ 
      
 
  function renderButtons() {
@@ -55,7 +42,7 @@
     		var results = response.data;
 
           	for (var s = 0; s < results.length; s++) {
-            	var gifDiv = $("<div class='item'>");
+            	var gifDiv = $("<div class='item' id='fixed'>");
 
             	var rating = results[s].rating;
 
@@ -71,15 +58,6 @@
           		} 
        		});
   		});
-	
-	// $("#prezGif").on("click", .function(event) {
-	// 	event.preventDefault();
-	// 	var newPrez = $("#addAPrez").val().trim();
-        	
- //        topics.push(newPrez);
- //        console.log(topics);
- //        renderButtons();
-	//  	});	
 	$("#addAPrezButton").on("click", function(event) {
 		event.preventDefault();
 		var newPrez = $("#addAPrez").val().trim();
@@ -88,6 +66,23 @@
         console.log(topics);
         renderButtons();
 	 	});	
+	$(document).on("click", "gifSpace", function() {
+	//$(".fixed").on("click", function() {
+		console.log('holla')
+      // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
+   var state = $(this).attr("id");
+   console.log(state)
+      // If the clicked image's state is still, update its src attribute to what its data-animate value is.
+      // Then, set the image's data-state to animate
+      // Else set src to the data-still value
+      // if (state === "still") {
+      //   $(this).attr("src", $(this).attr("data-animate"));
+      //   $(this).attr("data-state", "animate");
+      // } else {
+      //   $(this).attr("src", $(this).attr("data-still"));
+      //   $(this).attr("data-state", "still");
+      // }
+    });
 	});
 
       
